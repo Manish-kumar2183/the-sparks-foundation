ed5b33cb813720d9afe2d2a852e5b832dc60f2d9
manish kumar <65436604+Manish-kumar2183@users.noreply.github.com>
2021-08-19 23:51:58 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+$host="localhost";
+$user="id17424675_tsf_intern";
+$password="ku+A|d2Imnb(fLwg";
+$db="id17424675_tsf";
+	$link=mysqli_connect($host,$user,$password);
+    mysqli_select_db($link,$db);
+
+
+?>
---
+++ navbar.php
@@ -0,0 +1,20 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+       <a class="nav-brand" href="index.php"><img src="img/home.png" alt="Home" title="Home"></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+            
+              <li class="nav-item">
+                <a class="nav-link" href="createuser.php"><img src="img/user3-icon.jpg" alt="Create user" title="Create user"></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php"><img src="img/transfer-icon.jpg" alt="Transfer money" title="Transfer money"></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php"><img src="img/history-icon.jpg" alt="Transaction history" title="Transaction history"></a>
+              </li>
+          </div>
+       </nav>
